44820731a238355872821907980e1abd96e235ff
Ashish Kumar Singh <singhashish@microsoft.com>
2024-01-05 16:00:43 +0530

    Fixing datagrid control

--- Win11ThemeSampleApp/MainWindow.xaml.cs
+++ Win11ThemeSampleApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
+using System.Windows.Appearance;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -19,6 +20,38 @@ namespace Win11ThemeSampleApp
         public MainWindow()
         {
             InitializeComponent();
+            SetupDatagrid();
         }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ApplicationTheme theme = ApplicationThemeManager.GetAppTheme();
+            if (theme == ApplicationTheme.Light)
+            {
+                ApplicationThemeManager.Apply(ApplicationTheme.Dark);
+            }
+            else
+            {
+                ApplicationThemeManager.Apply(ApplicationTheme.Light);
+            }       
+
+        }
+
+        private void SetupDatagrid()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users;
+        }
+    }
+
+    internal class User
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public DateTime Birthday { get; internal set; }
     }
 }
