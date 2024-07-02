8f4f75a93b20e054b42b1205d080a9075bc7d65e
Vidyashree M B <v-vmb@microsoft.com>
2024-07-02 17:51:54 +0530

    Adding radio button cases

--- TestingApplication/MainWindow.xaml.cs
+++ TestingApplication/MainWindow.xaml.cs
@@ -23,69 +23,92 @@ namespace TestingApplication
             InitializeComponent();
         }
 
-        private void clickEvent(object sender, RoutedEventArgs e)
+        private void ClickEvent(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Button Clicked");
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //  this.NavigationService.Navigate(new Uri("Page3.xaml", UriKind.Relative));         
-            ButtonWindow btnwindow = new ButtonWindow();
-            btnwindow.Owner = this;
+            ButtonWindow btnwindow = new()
+            {
+                Owner = this
+            };
             //Added for fixing blank window issue
             Application.Current.MainWindow = btnwindow;         
             btnwindow.Show();
         }
 
-        private void calendarButton_Click(object sender, RoutedEventArgs e)
-        {          
-            CalendarWindow clndrwindow = new CalendarWindow();
-            clndrwindow.Owner = this;
+        private void CalendarButton_Click(object sender, RoutedEventArgs e)
+        {
+            CalendarWindow clndrwindow = new()
+            {
+                Owner = this
+            };
             //Added for fixing blank window issue
             Application.Current.MainWindow = clndrwindow;
             clndrwindow.Show();
         }
 
-        private void textBoxButton_Click(object sender, RoutedEventArgs e)
+        private void TextBoxButton_Click(object sender, RoutedEventArgs e)
         {
-            TextWindow tw = new TextWindow();
-            tw.Owner = this;
+            TextWindow tw = new()
+            {
+                Owner = this
+            };
             Application.Current.MainWindow = tw;
             tw.Show();
         }
 
-        private void comboBoxButton_Click(object sender, RoutedEventArgs e)
+        private void ComboBoxButton_Click(object sender, RoutedEventArgs e)
         {
-            ComboBoxViewModel vm = new ComboBoxViewModel();
-            ComboBoxWindow cbw = new ComboBoxWindow(vm);
-            cbw.Owner = this;
+            ComboBoxViewModel vm = new();
+            ComboBoxWindow cbw = new(vm)
+            {
+                Owner = this
+            };
+            Application.Current.MainWindow = cbw;
             cbw.Show();
         }
 
-        private void checkboxButton_Click(object sender, RoutedEventArgs e)
+        private void CheckboxButton_Click(object sender, RoutedEventArgs e)
         {
-            CheckBoxViewModel viewModel=new CheckBoxViewModel();
-            CheckboxWindow checkboxWindow = new CheckboxWindow(viewModel);
+            CheckBoxViewModel viewModel = new();
+            CheckboxWindow checkboxWindow = new(viewModel);
             //Added for fixing blank window issue
             Application.Current.MainWindow = checkboxWindow;
             checkboxWindow.Owner = this;
             checkboxWindow.Show();
         }
 
-        private void listboxButton_Click(object sender, RoutedEventArgs e)
+        private void ListboxButton_Click(object sender, RoutedEventArgs e)
         {
-            ListboxWindow lstbxwindow = new ListboxWindow();
-            lstbxwindow.Owner = this;
+            ListboxWindow lstboxwindow = new()
+            {
+                Owner = this
+            };
             //Added for fixing blank window issue
-            Application.Current.MainWindow = lstbxwindow;
-            lstbxwindow.Show();
+            Application.Current.MainWindow = lstboxwindow;
+            lstboxwindow.Show();
         }
-        private void sliderButton_Click(object sender, RoutedEventArgs e)
+        private void SliderButton_Click(object sender, RoutedEventArgs e)
         {
-            SliderWindow sliderWindow = new SliderWindow();
-            sliderWindow.Owner = this;
+            SliderWindow sliderWindow = new()
+            {
+                Owner = this
+            };
             Application.Current.MainWindow = sliderWindow;
             sliderWindow.Show();
+        }
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButtonWindow radioButtonWindow = new()
+            {
+                Owner = this
+            };
+            //Added for fixing blank window issue
+            Application.Current.MainWindow = radioButtonWindow;
+            radioButtonWindow.Show();
         }
     }
 }
