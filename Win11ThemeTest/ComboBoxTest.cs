c6b5102e1c405ec31985029314059c6226cdae10
Anjali Sheel <anjalisheel@microsoft.com>
2024-07-02 21:01:56 +0530

    Refined ComboBox tests

--- Win11ThemeTest/ComboBoxTest.cs
+++ Win11ThemeTest/ComboBoxTest.cs
@@ -9,60 +9,70 @@ namespace Win11ThemeTest
 {
     public class ComboBoxTests
     {
-        private Application? app;
-        private Window? mainWindow;
-        public Window? comboWindow;
-        Button? comboBoxButton;
-        ComboBox? comboBox;
-        ComboBox? comboBoxEditable;
-        ComboBox? comboBoxBind;
+        private readonly Application? app;
+        private readonly Window? mainWindow;
+        private readonly Window? comboWindow;
+        private readonly Button? comboBoxButton;
+        private readonly ComboBox? comboBox;
+        private readonly ComboBox? comboBoxEditable;
+        private readonly ComboBox? comboBoxBind;
+
         public ComboBoxTests()
+        {
+            var appPath = ConfigurationManager.AppSettings["Testpath"];
+            app = LaunchApplication(appPath);
+            using var automation = new UIA3Automation();
+            mainWindow = app?.GetMainWindow(automation);
+
+            comboBoxButton = mainWindow?.FindFirstDescendant(cf => cf.ByAutomationId("cmbBoxButton")).AsButton();
+            ClickButton(comboBoxButton);
+
+            comboWindow = mainWindow?.FindFirstDescendant(cf => cf.ByName("ComboBoxWindow")).AsWindow();
+            comboBox = comboWindow?.FindFirstDescendant(cf => cf.ByAutomationId("comboBoxList")).AsComboBox();
+            comboBoxEditable = comboWindow?.FindFirstDescendant(cf => cf.ByAutomationId("comboBoxEditable")).AsComboBox();
+            comboBoxBind = comboWindow?.FindFirstDescendant(cf => cf.ByAutomationId("comboBoxBind")).AsComboBox();
+        }
+
+        private Application? LaunchApplication(string? appPath)
         {
             try
             {
-                var appPath = ConfigurationManager.AppSettings["Testpath"];
-                app = Application.Launch(appPath);
-                using var automation = new UIA3Automation();
-                mainWindow = app.GetMainWindow(automation);
-                comboBoxButton = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("cmbBoxButton")).AsButton();
-                Mouse.Click(comboBoxButton.GetClickablePoint());
-                Wait.UntilInputIsProcessed(TimeSpan.FromMilliseconds(500));
-                comboWindow = mainWindow.FindFirstDescendant(cf => cf.ByName("ComboBoxWindow")).AsWindow();
-                comboBox = comboWindow.FindFirstDescendant(cf => cf.ByAutomationId("comboBoxList")).AsComboBox();
-                comboBoxEditable = comboWindow.FindFirstDescendant(cf => cf.ByAutomationId("comboBoxEditable")).AsComboBox();
-                comboBoxBind = comboWindow.FindFirstDescendant(cf => cf.ByAutomationId("comboBoxBind")).AsComboBox();
+                return Application.Launch(appPath);
             }
             catch (Exception ex)
             {
-                var filePath = ConfigurationManager.AppSettings["logpath"];
-                if (filePath != null)
-                {
-                    if (!Directory.Exists(filePath))
-                    {
-                        Directory.CreateDirectory(filePath);
-                    }
-                    filePath = filePath + "log_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";   //Text File Name
-                    if (!File.Exists(filePath))
-                    {
-                        File.Create(filePath).Dispose();
-                    }
-                    using StreamWriter sw = File.AppendText(filePath);
-                    string error = "Log Written Date:" + " " + DateTime.Now.ToString() + "\nError Message:" + " " + ex.Message.ToString();
-                    sw.WriteLine("-----------Exception Details on " + " " + DateTime.Now.ToString() + "-----------------");
-                    sw.WriteLine("-------------------------------------------------------------------------------------");
-                    sw.WriteLine(error);
-                    sw.Flush();
-                    sw.Close();
-                }
-                else
-                {
-                    throw new ArgumentNullException();
-                }
+                LogException(ex);
+                throw;
             }
         }
 
+        private void ClickButton(Button? button)
+        {
+            if (button == null) throw new ArgumentNullException(nameof(button));
+
+            Mouse.Click(button.GetClickablePoint());
+            Wait.UntilInputIsProcessed(TimeSpan.FromMilliseconds(500));
+        }
+
+        private void LogException(Exception ex)
+        {
+            var filePath = ConfigurationManager.AppSettings["logpath"];
+            if (string.IsNullOrEmpty(filePath)) throw new ArgumentNullException(nameof(filePath));
+
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            var logFilePath = Path.Combine(filePath, $"log_{DateTime.Now:yyyyMMddHHmmss}.txt");
+            using StreamWriter sw = new(logFilePath, append: true);
+            sw.WriteLine("-----------Exception Details on " + DateTime.Now + "-----------------");
+            sw.WriteLine("-------------------------------------------------------------------------------------");
+            sw.WriteLine($"Log Written Date: {DateTime.Now}\nError Message: {ex.Message}");
+        }
+
         [Test]
-        public void ComboBox_isAvailable()
+        public void ComboBox_IsAvailable()
         {
             Assert.Multiple(() =>
             {
@@ -74,95 +84,91 @@ namespace Win11ThemeTest
         }
 
         [Test]
-        public void ComboBox_selectedDefaultItem()
+        public void ComboBox_SelectedDefaultItem()
         {
             Assert.That(comboBox, Is.Not.Null);
-            Assert.That(comboBox.SelectedItem, Is.Not.Null);
+            Assert.That(comboBox?.SelectedItem, Is.Not.Null);
         }
 
         [Test]
-        public void ComboBox_isNull()
+        public void ComboBox_IsNotNull()
         {
             Assert.That(comboBox, Is.Not.Null);
         }
 
         [Test]
-        public void ComboBox_isReadableEditable()
+        public void ComboBox_IsReadableEditable()
         {
             Assert.That(comboBox, Is.Not.Null);
             Assert.Multiple(() =>
             {
-                Assert.That(comboBox.IsReadOnly, Is.False);
-                Assert.That(comboBox.IsEditable, Is.False);
-
+                Assert.That(comboBox?.IsReadOnly, Is.False);
+                Assert.That(comboBox?.IsEditable, Is.False);
             });
             Assert.That(comboBoxEditable, Is.Not.Null);
-            Assert.That(comboBoxEditable.IsEditable, Is.True);
+            Assert.That(comboBoxEditable?.IsEditable, Is.True);
         }
 
         [Test]
-        public void ComboBox1_selectItem()
+        public void ComboBox1_SelectItem()
         {
             Assert.That(comboBox, Is.Not.Null);
-            comboBox.Select("Red");
-            Assert.That(comboBox.SelectedItem.Name, Is.EqualTo("Red"));
+            comboBox?.Select("Red");
+            Assert.That(comboBox?.SelectedItem.Name, Is.EqualTo("Red"));
         }
 
-        //Check if the drop-down is open by clicking on both the drop-down & the drop-down Arrow.
         [Test]
-        public void ComboBox1_expandCollapse()
+        public void ComboBox1_ExpandCollapse()
         {
             Assert.That(comboBox, Is.Not.Null);
-            comboBox.Expand();
-            Assert.That(comboBox.ExpandCollapseState, Is.EqualTo(ExpandCollapseState.Expanded));
-            comboBox.Collapse();
-            Assert.That(comboBox.ExpandCollapseState, Is.EqualTo(ExpandCollapseState.Collapsed));
+            comboBox?.Expand();
+            Assert.That(comboBox?.ExpandCollapseState, Is.EqualTo(ExpandCollapseState.Expanded));
+            comboBox?.Collapse();
+            Assert.That(comboBox?.ExpandCollapseState, Is.EqualTo(ExpandCollapseState.Collapsed));
         }
 
         [Test]
-        public void ComboBox2_editableText()
+        public void ComboBox2_EditableText()
         {
             Assert.That(comboBoxEditable, Is.Not.Null);
-            Assert.That(comboBoxEditable.IsEditable, Is.True);
             comboBoxEditable.EditableText = "10";
-            Assert.That(comboBoxEditable.SelectedItem, Is.Not.Null);
-            Assert.That(comboBoxEditable.SelectedItem.Text, Is.EqualTo("10"));
+            Assert.That(comboBoxEditable?.SelectedItem, Is.Not.Null);
+            Assert.That(comboBoxEditable?.SelectedItem.Text, Is.EqualTo("10"));
         }
 
-        //Check whether the dropdown is clickable or not.
         [Test]
-        public void ComboBox3_mouseClick()
+        public void ComboBox3_MouseClick()
         {
             Assert.That(comboBox, Is.Not.Null);
-            comboBox.Click();
-            Assert.That(comboBox.ExpandCollapseState, Is.EqualTo(ExpandCollapseState.Expanded));
-            comboBox.Collapse();
+            comboBox?.Click();
+            Assert.That(comboBox?.ExpandCollapseState, Is.EqualTo(ExpandCollapseState.Expanded));
+            comboBox?.Collapse();
         }
 
         [Test]
-        public void ComboBox4_mouseSelectClick()
+        public void ComboBox4_MouseSelectClick()
         {
             Assert.That(comboBox, Is.Not.Null);
             Mouse.MoveTo(comboBox.GetClickablePoint());
             Mouse.Click();
             Wait.UntilInputIsProcessed(TimeSpan.FromMilliseconds(500));
-            comboBox.Items[3].Click();
-            Assert.That(comboBox.SelectedItem.Name, Is.EqualTo("Yellow"));
+            comboBox?.Items[3].Click();
+            Assert.That(comboBox?.SelectedItem.Name, Is.EqualTo("Yellow"));
         }
 
         [Test]
-        public void ComboBox5_cleanUp()
+        public void ComboBox5_CleanUp()
         {
             if (app != null)
             {
                 app.Close();
+                Assert.That(app.Close(), Is.True);
                 Console.WriteLine("Application closed successfully.");
-                Assert.That(app.Close());
             }
             else
             {
                 Console.WriteLine("Application not found.");
-                Assert.That(app.Close());
+                Assert.Fail("Application not found.");
             }
         }
     }
